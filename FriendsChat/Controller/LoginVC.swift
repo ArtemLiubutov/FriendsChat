cadbd8962a127793538171f2ce684a7f3af60177
artemLiubutov <lubutov033@gmail.com>
2017-10-18 23:55:27 +0500

    add login screen

---
+++ FriendsChat/Controller/LoginVC.swift
@@ -0,0 +1,24 @@
+//
+//  LoginVC.swift
+//  FriendsChat
+//
+//  Created by Artem Lubytov on 18.10.17.
+//  Copyright © 2017 RocketApp. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+   
+    @IBAction func closeBtnPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ FriendsChat/Utilities/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  FriendsChat
+//
+//  Created by Artem Lubytov on 18.10.17.
+//  Copyright © 2017 RocketApp. All rights reserved.
+//
+
+import Foundation
+
+let TO_LOGIN = "toLogin"
